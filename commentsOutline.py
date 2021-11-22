d39b630f4d3decec5664456771f2cc4ec7ee51fb
jaqx-kanuhsa <94816119+jaqx-kanuhsa@users.noreply.github.com>
2021-11-21 22:35:38 -0600

    Add files via upload

---
+++ commentsOutline.py
@@ -0,0 +1,19 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+
+#Hi???
+
+#2) Make a multi-line comment.
+
+'''
+Brrrrrrr
+'''
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+a = 5 #Hi
+
